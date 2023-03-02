044166aa50ccef07eb4633f25c329d3606af0d4f
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-03-02 15:30:05 +0200

    first

---
+++ src/data.js
@@ -0,0 +1,32 @@
+
+const data = [
+    {
+        "id": "1",
+        "name": "Alex",
+        "age": "39",
+        "email": "alex.lux7777777@gmail.com",
+        "country": "Ukraine",
+        "company": "Deep Dive",
+        "birthday": "01.08.1983"
+    },
+    {
+        "id": "2",
+        "name": "Anna",
+        "age": "39",
+        "email": "anna.lux777@gmail.com",
+        "country": "Ukraine",
+        "company": "Mir Kvartir Ukraine",
+        "birthday": "11.01.1984"
+    },
+    {
+        "id": "3",
+        "name": "Nikita",
+        "age": "15",
+        "email": "nikita.lux777@gmail.com",
+        "country": "Ukraine",
+        "company": "Football World",
+        "birthday": "28.05.2007"
+    }
+];
+
+module.exports = data;
---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import axios from "axios";
+
+// ! ДЗ 49. Работа с webpack, git
+
+// 1) Создать проект на webpack, webpack-cli, webpack-dev-server по видео текущего урока.
+// 2) Создать массив с пользователями в файле data
+// 3) Создать файлЫ с функциями которая выводит пользователей
+//    на страницу ввиде списка из файла data.js:
+//    id, name, age, email, country, company, birthday
+// 4) Создать репозиторий на git и выгрузить туда проект
+
+const container = document.getElementById("container");
+
+
+async function getData() {
+    const users = require('./data');
+    console.log(users);
+    users.forEach(item => {
+        // console.log(item);
+        renderUser(item);
+    })
+
+}
+getData();
+
+function renderUser(user) {
+    console.log(user);
+    const box = document.createElement('div');
+    box.classList.add("box");
+    const infoUser = document.createElement('p').innerText = `Имя: ${user.name}, возраст: ${user.age},
+    страна: ${user.country}, дата рождения: ${user.birthday}, email: ${user.email}, компания: ${user.company}`;
+
+    box.append(infoUser);
+    container.append(box);
+}
+console.log(777);
